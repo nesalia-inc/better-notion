3a4962dbd8686b5405211d353429beadfb8c2148
Dave <davidddpereiraaa6@gmail.com>
2026-01-25 15:06:53 +0100

    feat: update Block.from_data() to support all block types
    Update Block.from_data() factory method to instantiate all 25+ block types:
    
    - Text blocks: paragraph, heading_1/2/3, code, quote
    - List blocks: bulleted_list_item, numbered_list_item, to_do, toggle
    - Media blocks: image, video, audio, file, pdf
    - Embed blocks: bookmark, embed, equation
    - Layout blocks: divider, callout, column_list, column
    - Table blocks: table, table_row
    - Special blocks: breadcrumb, synced_block, template
    
    The factory now correctly maps Notion block types to their specialized
    Python classes, enabling automatic type-specific functionality.

--- better_notion/_sdk/models/block.py
+++ better_notion/_sdk/models/block.py
@@ -84,31 +84,71 @@ class Block(BaseEntity):
         Example:
             >>> block = Block.from_data(client, api_data)
         """
-        from better_notion._sdk.models.blocks.code import Code
-        from better_notion._sdk.models.blocks.todo import Todo
-        from better_notion._sdk.models.blocks.paragraph import Paragraph
-        from better_notion._sdk.models.blocks.heading import Heading
+        from better_notion._sdk.models.blocks.audio import Audio
+        from better_notion._sdk.models.blocks.bookmark import Bookmark
+        from better_notion._sdk.models.blocks.breadcrumb import Breadcrumb
         from better_notion._sdk.models.blocks.bullet import Bullet
-        from better_notion._sdk.models.blocks.numbered import Numbered
-        from better_notion._sdk.models.blocks.quote import Quote
-        from better_notion._sdk.models.blocks.divider import Divider
         from better_notion._sdk.models.blocks.callout import Callout
+        from better_notion._sdk.models.blocks.code import Code
+        from better_notion._sdk.models.blocks.column import Column
+        from better_notion._sdk.models.blocks.column_list import ColumnList
+        from better_notion._sdk.models.blocks.divider import Divider
+        from better_notion._sdk.models.blocks.embed import Embed
+        from better_notion._sdk.models.blocks.equation import Equation
+        from better_notion._sdk.models.blocks.file import FileBlock
+        from better_notion._sdk.models.blocks.heading import Heading
+        from better_notion._sdk.models.blocks.image import Image
+        from better_notion._sdk.models.blocks.numbered import Numbered
+        from better_notion._sdk.models.blocks.paragraph import Paragraph
+        from better_notion._sdk.models.blocks.pdf import PDF
+        from better_notion._sdk.models.blocks.quote import Quote
+        from better_notion._sdk.models.blocks.synced_block import SyncedBlock
+        from better_notion._sdk.models.blocks.table import Table
+        from better_notion._sdk.models.blocks.table_row import TableRow
+        from better_notion._sdk.models.blocks.template import Template
+        from better_notion._sdk.models.blocks.todo import Todo
+        from better_notion._sdk.models.blocks.toggle import Toggle
+        from better_notion._sdk.models.blocks.video import Video
 
         block_type = data.get("type", "")
 
         # Map to specialized class
         block_classes = {
-            "code": Code,
-            "to_do": Todo,
+            # Text blocks
             "paragraph": Paragraph,
             "heading_1": Heading,
             "heading_2": Heading,
             "heading_3": Heading,
+            "code": Code,
+            "quote": Quote,
+            # List blocks
             "bulleted_list_item": Bullet,
             "numbered_list_item": Numbered,
-            "quote": Quote,
+            "to_do": Todo,
+            "toggle": Toggle,
+            # Media blocks
+            "image": Image,
+            "video": Video,
+            "audio": Audio,
+            "file": FileBlock,
+            "pdf": PDF,
+            # Embed blocks
+            "bookmark": Bookmark,
+            "embed": Embed,
+            "equation": Equation,
+            # Layout blocks
             "divider": Divider,
             "callout": Callout,
+            "quote": Quote,
+            "column_list": ColumnList,
+            "column": Column,
+            # Table blocks
+            "table": Table,
+            "table_row": TableRow,
+            # Special blocks
+            "breadcrumb": Breadcrumb,
+            "synced_block": SyncedBlock,
+            "template": Template,
         }
 
         block_class = block_classes.get(block_type, cls)
